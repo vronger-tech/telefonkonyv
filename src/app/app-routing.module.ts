01254c2d615205e2d7a2c552973a970881450389
vronger-tech <stamas.egy@gmail.com>
2022-03-17 22:48:58 +0100

    menuok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ApiComponent } from './api/api.component';
+import { F1Component } from './f1/f1.component';
+import { F2Component } from './f2/f2.component';
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {path:'api',component:ApiComponent},
-  {path:'**',component:ApiComponent}
+  {path:'telefonkonyv',component:ApiComponent},
+  {path:'feladat1',component:F1Component},
+  {path:'feladat2',component:F2Component},
+  {path:'Home',component:HomeComponent},
+  {path:'**',component:HomeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,60 @@ import { ApiComponent } from './api/api.component';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+import { F1Component } from './f1/f1.component';
+import { F2Component } from './f2/f2.component';
+import { HomeComponent } from './home/home.component';
+
+
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {matSortAnimations, MatSortModule} from '@angular/material/sort';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatSelectModule} from '@angular/material/select';
+import {matDatepickerAnimations, MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDividerModule} from '@angular/material/divider';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    ApiComponent
+    ApiComponent,
+    F1Component,
+    F2Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     CommonModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatStepperModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatDatepickerModule,
+MatDividerModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
